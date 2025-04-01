57987a723f2991c55e77df869897e4ed1de25660
sw33tdreams <artyom.hakobyanshnik@gmail.com>
2025-04-01 13:32:49 +0400

    valid task 2

---
+++ task2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int arr[10] =  {};
+	printf("enter your number of numbers:  ");
+	int num = 0;
+	scanf("%d",&num);
+	int l = 0;
+	printf("enter %d int numbers:  ",num);
+	for(int i = 0; i < num; ++i){
+		scanf("%d",arr + i);}
+	for(int i = 0; i < num; ++i){
+		if(*(arr + i) % 2){
+		       ++l;}
+	}
+       printf("num of kent tver is %d\n",l);
+	return 0;
+}		
